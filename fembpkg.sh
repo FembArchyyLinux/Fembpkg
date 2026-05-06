d77eac0902ff842bbc86144771985b228661ad12
Fembarch <clubconnection0@gmail.com>
2026-05-06 08:07:06 +0300

    Fix quoting issues in fembpkg.sh commands

--- fembpkg.sh
+++ fembpkg.sh
@@ -4,18 +4,18 @@ key="$1"
 
 if [[ "$key" == "femb-get" ]]; then
     PACKAGE="$2"
-    sudo pacman -S $2
+    sudo pacman -S "$2"
 elif [[ "$key" == "femb-search" ]]; then
     SEARCH_PKG="$2"
-    sudo pacman -Ss $2
+    sudo pacman -Ss "$2"
 elif [[ "$key" == "femb-remove" ]]; then
     REMOVE="$2"
-    sudo pacman -Rns $2
+    sudo pacman -Rns "$2"
 else
     if [[ -z "$key" ]]; then
         echo "" 
     else 
-        echo "Inavlid option: $1"
+        echo "Invalid option: $1"
     fi
 fi
 
